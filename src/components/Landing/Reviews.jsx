9227066b2e757513ed6b359f902dd9da6e8af489
Alekssin1 <xomkaxomiakovuch@gmail.com>
2024-03-24 15:04:54 +0200

    upd-landing

--- src/components/Landing/Reviews.jsx
+++ src/components/Landing/Reviews.jsx
@@ -2,8 +2,9 @@ import React, { useState } from 'react';
 import '../../styles/Reviews.scss'
 import ReviewCard from '../UI/ReviewCard';
 import { Element } from 'react-scroll';
-import { ReactComponent as NextArrowSVG} from '../../assets/images/Reviews/arrow-left.svg';
-import { ReactComponent as PreviousArrowSVG} from '../../assets/images/Reviews/arrow-right.svg';
+import { ReactComponent as PreviousArrowSVG} from '../../assets/images/Reviews/arrow-left.svg';
+import { ReactComponent as NextArrowSVG} from '../../assets/images/Reviews/arrow-right.svg';
+import Slider from "react-slick";
 
 
 const reviews = [
@@ -20,42 +21,64 @@ const reviews = [
     },
     {
       name: "Степан",
-      text: "Найсмачніший шашлик, який я їв на даний моментfdsafffffff ffffffffffffffffffffffffffffffff fffffffffff fffffffff.",
+      text: "Найсмачніший шашлик, який я їв на даний момент",
       rating: 5,
     },
-    // {
-    //   name: "Олег",
-    //   text: "Найсмачніший шашлик, який я їв на даний моментfdsafffffff ffffffffffffffffffffffffffffffff fffffffffff fffffffff.",
-    //   rating: 5,
-    // },
-    // {
-    //   name: "Інна",
-    //   text: "Найсмачніший шашлик, який я їв на даний моментfdsafffffff ffffffffffffffffffffffffffffffff fffffffffff fffffffff.",
-    //   rating: 4,
-    // },
-    // {
-    //   name: "Джеремі",
-    //   text: "Найсмачніший шашлик, який я їв на даний моментfdsafffffff ffffffffffffffffffffffffffffffff fffffffffff fffffffff.",
-    //   rating: 4,
-    // },
-    // {
-    //   name: "Оля",
-    //   text: "Найсмачніший шашлик, який я їв на даний моментfdsafffffff ffffffffffffffffffffffffffffffff fffffffffff fffffffff.",
-    //   rating: 5,
-    // },
+    {
+      name: "Олег",
+      text: "Найсмачніший шашлик, який я їв на даний момент",
+      rating: 5,
+    },
+    {
+      name: "Інна",
+      text: "Найсмачніший шашлик, який я їв на даний момент",
+      rating: 4,
+    },
+    {
+      name: "Джеремі",
+      text: "Найсмачніший шашлик, який я їв на даний момент",
+      rating: 4,
+    },
+    {
+      name: "Оля",
+      text: "Найсмачніший шашлик, який я їв на даний момент",
+      rating: 5,
+    },
   ];
 
 function Reviews() {
 
 const [currentIndex, setCurrentIndex] = useState(reviews.findIndex(review => review.current));
+const settings = {
+  className: "center",
+  centerMode: true,
+  infinite: true,
+  slidesToShow: 3,
+  centerPadding: "20px",
+  speed: 600,
+  nextArrow: <NextArrowSVG className="arrow-controller"  />,
+  prevArrow: <PreviousArrowSVG className="arrow-controller" />,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+      }
+    },
+    {
+      breakpoint: 886,
+      settings: {
+        slidesToShow: 1,
+      }
+    },
+  ]
+};
 
   return (
     <Element name="reviews" className='reviews'>
         <h1>Що кажуть про нас клієнти</h1>
-        <NextArrowSVG className = "reviews__next-slide" />
-        <PreviousArrowSVG className = "reviews__previous-slide"/>
-        <div className="reviews__content">
-            <div className="reviews__card-container">
+        <div className="slider-container">
+          <Slider {...settings}>
                 {reviews.map((review, index) => (
                     <ReviewCard
                         key={index}
@@ -65,9 +88,9 @@ const [currentIndex, setCurrentIndex] = useState(reviews.findIndex(review => rev
                         current={index === currentIndex}
                     />
                 ))}
-            </div>
-        
+          </Slider>
         </div>
+        
     </Element>
   )
 }
--- src/components/UI/ReviewCard.jsx
+++ src/components/UI/ReviewCard.jsx
@@ -5,17 +5,17 @@ import { ReactComponent as User} from '../../assets/images/Reviews/user.svg';
 import { ReactComponent as StarIcon} from '../../assets/images/Reviews/star.svg'; 
 import { ReactComponent as UnactiveStarIcon} from '../../assets/images/Reviews/unactive-star.svg';
 
-const ReviewCard = ({ name, text, rating, current = false  }) => {
+const ReviewCard = ({ name, text, rating  }) => {
   return (
-    <div className={`reviews__card ${current ? 'reviews__current' : 'reviews__not-current'} `}>
+    <div className={`reviews__card `}>
       <User className="reviews__user-pic" />
       <Box component="fieldset" borderColor="transparent" sx={{ '& .MuiRating-icon': { marginRight: '0.5rem', fontSize: '1rem' } }}>
         <Rating
           name="read-only"
           value={rating}
           readOnly
-          icon={<StarIcon className={`reviews__star ${current ? 'reviews__current-star' : ''}`} />}
-          emptyIcon={<UnactiveStarIcon className={`reviews__star ${current ? 'reviews__current-star' : ''}`} />}
+          icon={<StarIcon className={`reviews__star`} />}
+          emptyIcon={<UnactiveStarIcon className={`reviews__star`} />}
         />
       </Box>
       <div className="reviews__comment-content">
