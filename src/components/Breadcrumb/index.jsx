b3fa4318e9b55a941d53c712d0e7aa0d7ed33ad4
Alekssin1 <xomkaxomiakovuch@gmail.com>
2024-04-01 02:30:09 +0300

    fixed breadcrumb ghpages

--- src/components/Breadcrumb/index.jsx
+++ src/components/Breadcrumb/index.jsx
@@ -4,6 +4,7 @@ import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
 import Stack from '@mui/material/Stack';
 import "components/Breadcrumb/index.scss"
+import "styles/Colors.scss"
 
 
 function handleClick(event) {
@@ -34,7 +35,58 @@ export default function Breadcrumb() {
         <>  
         <div className="bread-crumb">
             <Stack spacing={2}>
-                <Breadcrumbs separator="-" aria-label="breadcrumb">
+                <Breadcrumbs separator="-" aria-label="breadcrumb" 
+                sx={{
+                    ".css-bvyohc-MuiTypography-root-MuiLink-root": {
+                        color: "#fff !important",
+                        opacity: "0.8",
+                        fontSize: "0.75rem!important",
+                        letterSpacing: "0.1em!important",
+                        fontWeight: "500!important",
+                    },
+                    
+                    ".css-bvyohc-MuiTypography-root-MuiLink-root:hover": {
+                        color: "#a63d40!important",
+                        textDecoration: "underline",
+                    },
+                    
+                    ".css-1wuw8dw-MuiBreadcrumbs-separator": {
+                        color: "#fff !important",
+                        opacity: "0.6",
+                    },
+                    
+                    ".css-4pdmu4-MuiBreadcrumbs-ol": {
+                        marginLeft: "7rem!important",
+                        marginTop: "0.9375rem!important",
+                        marginBottom: "0.6875rem!important",
+                        backgroundColor: "rgba(#121212, 0.4)!important",
+                        "@media screen and (max-width: 1200px)": {
+                            marginLeft: "4.5rem!important",
+                        },
+                        "@media screen and (max-width: 768px)": {
+                            marginLeft: "2rem!important",
+                        },
+                        "@media screen and (max-width: 540px)": {
+                            marginLeft: "1.25rem!important",
+                            marginTop: "0.625rem!important",
+                            marginBottom: "0.375rem!important",
+                        },
+                        
+                    },
+                    
+                    ".css-1gqyz35-MuiTypography-root": {
+                        justifyContent: "bottom!important",
+                        color: "#a63d40!important",
+                        fontFamily: "'Inter'!important",
+                        fontSize: "0.75rem!important",
+                        letterSpacing: "0.1em!important",
+                        fontWeight: "500!important",
+                        lineHeight: "1!important",
+                        textDecoration: "underline",
+                    },
+                }   
+                    
+                }>
                     {breadcrumbs}
                 </Breadcrumbs>
             </Stack>
